3ae647fc28972a2cfec83522998f8e548a639563
Dovi Ichilevici <dovi1499@gmail.com>
2022-01-13 20:02:08 -0500

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld{
+    public static void main(String[] args){
+        System.out.println("My name is Dovi Ichilevici");
+        System.out.println("I'm 22 years old");
+        System.out.println("My hometown is Aventura, FL");
+    }
+}
